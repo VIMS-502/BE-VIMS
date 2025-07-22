4fa4cb27af3a96a02853142cd068ffeb5d873962
noah <flhbigfamily7@gmail.com>
2025-07-22 12:30:20 +0900

    chore: 채팅 웹소켓 엔드 포인트 /ws/chat로 바꿈

--- backend/src/main/java/com/vims/webrtc/config/WebSocketConfig.java
+++ backend/src/main/java/com/vims/webrtc/config/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/ws/chat")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
